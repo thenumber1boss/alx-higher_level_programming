28d7c803602ccad8f6b6bb989eacf2a09037d551
thenumber1boss <bamijokoseun@gmail.com>
2023-03-09 22:37:22 +0000

    hot check

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#n!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
