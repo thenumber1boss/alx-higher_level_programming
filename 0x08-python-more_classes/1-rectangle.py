702339e3c2b916238fc9efe9e74e2886dacfe8bb
thenumber1boss <bamijokoseun@gmail.com>
2023-04-05 01:10:00 +0000

    yt

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,37 +7,55 @@ class Rectangle:
     ''' Class that defines a rectangle '''
 
 def __init__(self, width=0, height=0):
-    '''Initializing instances'''
+    """ Initializes a new instance of Rectangle.
+
+    Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
     self.width = width
     self.height = height
 
-def getWidth(self):
-    ''' method to return width value
-    Returns:
-        rectangle width
-    '''
+@property
+def width(self):
+    """ Gets the width of the rectangle. """
     return self.__width
 
-def setWidth(self, value):
+@width.setter
+def width(self, value):
+    """ Sets the width of the rectangle.
+
+    Args:
+        value (int): The value to set the width to.
+
+    Raises:
+        TypeError: If the value is not an int.
+        ValueError: If the value is negative.
+    """
     if not isinstance(value, int):
         raise TypeError("width must be an integer")
     if value < 0:
         raise ValueError("width must be >= 0")
     self.__width = value
 
-def getHeight(self):
-    ''' method to return height value
-    Returns:
-        rectangle height
-    '''
+@property
+def height(self):
+    """ Gets the height of the rectangle. """
     return self.__height
 
-def setHeight(self, value):
+@height.setter
+def height(self, value):
+    """ Sets the height of the rectangle.
+
+    Args:
+        value (int): The value to set the height to.
+
+    Raises:
+        TypeError: If the value is not an int.
+        ValueError: If the value is negative.
+    """
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
     if value < 0:
         raise ValueError("height must be >= 0")
     self.__height = value
-
-width = property(getWidth, setWidth)
-height = property(getHeight, setHeight)
